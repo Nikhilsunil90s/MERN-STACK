5075bab5139f5bcbc1d81004c239a0ea567e6319
unknown <nikhilsunil90s@gmail.com>
2021-02-03 12:26:59 +0530

    Some JavaScript Refreshers on Loops, Arrays and JSON

--- play.js
+++ play.js
@@ -1,1 +1,32 @@
-console.log("Hello, World");
+let person = { 
+    name : 'Max',
+    age : 29,
+    greet(){
+        console.log("Hello : " + this.name);
+    },
+}
+
+console.log(person.name);
+person.greet();
+
+let hobbies = ['programming' , 'sports' , 'cricket' , 'singing' , 'speaking']
+
+// for(let i = 0; i < hobbies.length ; i++){
+//     console.log(hobbies[i]);
+// }
+// for (let hobby of hobbies){
+//     console.log(hobby);
+// }
+function changeinput(arg){
+    return arg + '!';
+}
+//console.log(hobbies.map(changeinput) , hobbies);
+
+console.log(hobbies.map(arg => arg + '!'));
+
+
+
+
+
+
+
